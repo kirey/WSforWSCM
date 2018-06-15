afeffb033cd37ff90772bddf9b73cea2c13ef889
Vladimir Randjelovic <vladimirmicko@gmail.com>
2018-06-15 09:49:42 +0200

    Initial commit

---
+++ src/main/java/vladimir/websocket/CounterHandler.java
@@ -0,0 +1,69 @@
+package vladimir.websocket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class CounterHandler extends TextWebSocketHandler {
+
+    private WebSocketSession session;
+    private List<WebSocketSession> sessionList = new ArrayList(); 
+
+
+    // This will send only to one client(most recently connected)
+    public void counterIncrementedCallback(int counter) {
+        System.out.println("Trying to send:" + counter);
+        if (session != null && session.isOpen()) {
+            try {
+                System.out.println("Now sending:" + counter);
+                session.sendMessage(new TextMessage("{\"value\": \"" + counter + "\"}"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Don't have open session to send:" + counter);
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        System.out.println("Connection established");
+        System.out.println("-------------------------------------------------- "+ session.getHandshakeHeaders());
+        this.session = session;
+        sessionList.add(session);
+    }
+    
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        if ("CLOSE".equalsIgnoreCase(message.getPayload())) {
+            session.close();
+        } else {
+            System.out.println("Received:" + message.getPayload());
+        }
+    }
+
+	public WebSocketSession getSession() {
+		return session;
+	}
+
+	public void setSession(WebSocketSession session) {
+		this.session = session;
+	}
+
+	public List<WebSocketSession> getSessionList() {
+		return sessionList;
+	}
+
+	public void setSessionList(List<WebSocketSession> sessionList) {
+		this.sessionList = sessionList;
+	}
+
+
+}
---
+++ src/main/webapp/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { $WebSocket } from 'angular2-websocket/angular2-websocket';
+
+@Component({
+  selector: 'my-app',
+  template: `
+	Counter Value is: {{counter}}
+	<button type="button" (click)="subscribe($event)">Subscribe to WebSocket</button>
+	`
+})
+export class AppComponent {
+
+  counter: string = 'not known';
+  ws: any;
+  targetUrl: string = window.location.href.replace('http', 'ws') + 'counter'
+
+
+  constructor() {
+    this.ws = new $WebSocket(this.targetUrl);
+  }
+
+  subscribe($event) {
+    console.log("trying to subscribe to ws");
+    this.ws = new $WebSocket(this.targetUrl);
+
+    this.ws.send("Mario Petrovic is the BOSS");
+    this.ws.getDataStream().subscribe(
+      res => {
+        var count = JSON.parse(res.data).value;
+        console.log('Got: ' + count);
+        this.counter = count;
+      },
+      function (e) { console.log('Error: ' + e.message); },
+      function () { console.log('Completed'); }
+    );
+  }
+}
+
+
